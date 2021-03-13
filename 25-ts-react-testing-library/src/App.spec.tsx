2fb2bddd6dbb8493337b90ecdafd8a3752b6d7b8
mingyuchoo <mingyuchoo@gmail.com>
2021-03-13 09:05:01 +0900

    modified

--- 25-ts-react-testing-library/src/App.spec.tsx
+++ 25-ts-react-testing-library/src/App.spec.tsx
@@ -1,14 +1,23 @@
 /* eslint-disable react/display-name */
+/******************************************************************************
+ * REFERENCE WEB SITE
+ * https://medium.com/@ericdcobb/advanced-react-component-mocks-with-jest-and-react-testing-library-f1ae8838400b
+ *
+ ******************************************************************************/
 import { render, screen } from '@testing-library/react';
 import React from 'react';
 
 import App from './App';
 
-// Simple Inline Component Mocks
-// https://medium.com/@ericdcobb/advanced-react-component-mocks-with-jest-and-react-testing-library-f1ae8838400b
+// SIMPLE INLINE COMPONENT MOCKS
+// Component definition is missing display name but it works.
 jest.mock('./layout/Header', () => () => <header>Header</header>);
 jest.mock('./layout/Nav', () => () => <nav>Nav</nav>);
-jest.mock('./layout/Section', () => () => <section>Section</section>);
+jest.mock('./layout/Section', () => {
+  return () => {
+    return <section>Section</section>;
+  };
+});
 
 describe('<App />', () => {
   describe('This is not UNIT test.', () => {
